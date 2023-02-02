8bfe057d0579d7bcab76ba2414d053d7b5c6713a
JuanSoftware <js33neospro@gmail.com>
2023-02-02 15:33:39 -0400

    Adicionei os spwan poitns
    ainda não terminei o codigo

---
+++ Jogo02/Assets/scripts/FruitSpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject [] fruitsPrefabs;
+    [SerializeField] private Transform[] spwanPoints;
+    [SerializeField] private float minDelay,MaxDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+}
