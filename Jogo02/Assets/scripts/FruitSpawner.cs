a8f26f4f9e1972099749050916ef15da3da39ef6
JuanSoftware <js33neospro@gmail.com>
2023-03-05 09:46:51 -0400

    Criando spwan do objeto

--- Jogo02/Assets/scripts/FruitSpawner.cs
+++ Jogo02/Assets/scripts/FruitSpawner.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FruitSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject [] fruitsPrefabs;
+    [SerializeField] private GameObject [] fruitsPrefab;
     [SerializeField] private Transform[] spwanPoints;
-    [SerializeField] private float minDelay,MaxDelay;
+    [SerializeField] private float minDelay,maxDelay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Spawn());
     }
+    private IEnumerator Spawn(){
+        while(true){
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(delay);
 
-    // Update is called once per frame
-    
+            int spawnIndex = Random.Range(0,spwanPoints.Length);
+            Transform spawnPoint = spwanPoints[spawnIndex];
+            GameObject fruitPrefab = Instantiate(fruitsPrefab[Random.Range(0, fruitsPrefab.Length)], spawnPoint.position, spawnPoint.rotation);
+            Destroy(fruitPrefab, 5f);
+
+        }
+    }
+   
 }
