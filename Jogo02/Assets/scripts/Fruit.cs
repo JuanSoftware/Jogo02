bbd83aa3dd7543119649b54d3f73efe717df128c
JuanSoftware <js33neospro@gmail.com>
2023-03-11 15:49:06 -0400

    Efeito de splash dos objetos

--- Jogo02/Assets/scripts/Fruit.cs
+++ Jogo02/Assets/scripts/Fruit.cs
@@ -7,9 +7,11 @@ public class Fruit : MonoBehaviour
     private Rigidbody2D myRB;
     [SerializeField] private float startForce;
     public GameObject fruitSliced;
+    private GameController gameController;
     void Start()
     {
         myRB = this.gameObject.GetComponent<Rigidbody2D> ();
+        gameController = FindObjectOfType<GameController>();
         ApplyForce();
     }
 
@@ -20,5 +22,23 @@ public class Fruit : MonoBehaviour
     }
     private void ApplyForce(){
         myRB.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
+    public Color32 ChangeSplashColor(GameObject GO){
+        string cloneObjectName= GO.transform.name;
+        Color32 defaultColor = new Color32(255,255,255,255);
+        switch(cloneObjectName){
+            case "Apple(Clone)":
+            return gameController.appleColor;
+            case "Orange(Clone)":
+            return gameController.orangeColor;
+            case "Coconout(Clone)":
+            return gameController.coconutColor;
+            case "Pear(Clone)":
+            return gameController.pearColor;
+            case "Pineapple(Clone)":
+            return gameController.pineaplleColor;
+
+        }
+        return defaultColor;
     }
 }
---
+++ Jogo02/Assets/scripts/GameController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject splash;
+    [HideInInspector] public Color32 appleColor = new Color32(116, 144, 11,255), coconutColor = new Color32(109,69,43,255), pineaplleColor = new Color32(188,123,23,255), orangeColor = new Color32(255,131,13,255), pearColor = new Color32(174,185,0,255) ;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
